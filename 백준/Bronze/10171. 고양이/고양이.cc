6aef307706800e05e95f3dc1186b0c6667172d70
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-11-27 17:07:14 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	//특정 기능을 요하는 문자들을 출력하고자 하면 \를 추가해 조합하면 된다!
+	//ex) 백슬래시(\) 출력하려면 \\ 
+	
+	std::cout << "\\    /\\" << std::endl;
+	std::cout << " )  ( ')" << std::endl;
+	std::cout << "(  /  )" << std::endl;
+	std::cout << " \\(__)|" << std::endl;
+
+	return 0;
+}
