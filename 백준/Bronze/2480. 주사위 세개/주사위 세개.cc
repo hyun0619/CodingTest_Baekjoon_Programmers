2dbf83e8b7cd9a7e1f6231de591ec21be0a5b91a
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-11-27 20:31:21 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -4,15 +4,17 @@ int main() {
 
 	int A, B, C;
 	int max;
+
 	std::cin >> A >> B >> C;
 
-	// 3개 다 같은 눈이 나온 경우
+	// 3개가 다 같은 눈이 나온 경우
 	if (A == B && A == C) std::cout << 10000 + A * 1000 << std::endl;
-	// 2개만 같은 눈이 나온 경우
+
+	// 2개가 같은 눈이 나온 경우
 	else if (A == B || A == C) std::cout << 1000 + A * 100 << std::endl;
-	else if (B==C) std::cout << 1000 + B * 100 << std::endl;
-	
-	//1개만 같은 눈이 나온 경우
+	else if (B == C) std::cout << 1000 + B * 100 << std::endl;
+
+	// 모두 다른 눈이 나온 경우
 	else {
 		max = A;
 		if (A < B) {
@@ -21,7 +23,7 @@ int main() {
 		}
 		else if (A < C) max = C;
 		std::cout << max * 100 << std::endl;
-
 	}
+
 	return 0;
 }
