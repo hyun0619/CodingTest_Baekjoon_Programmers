54f3d127d15aa9197b13896881b34cef57204735
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-11-27 15:22:26 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,11 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int x,y,sum;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    sum = x-y;
+    printf("%d", sum);
+    return 0;
+}
