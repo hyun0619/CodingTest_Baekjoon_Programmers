e126e457412d62d96f5128ec05d30e4a37055995
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-11-27 16:58:05 +0900

    Add comment about long long data type usage
    Added a comment explaining the use of long long type.

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int main() {
-
+	//long long 자료형은 int형 연산에서 초과되는 범위를 다룰 때 사용
 	long long A, B, C;
 
 	std::cin >> A >> B >> C;
 	std::cout << A + B + C << std::endl;
 	return 0;
+
 }
