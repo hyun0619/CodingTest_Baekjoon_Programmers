ec2a3e102deaac296bb6af0d705ce73f74930751
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-11-27 17:23:02 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	//특정 기능을 요하는 문자들을 출력하고자 하면 \를 추가해 조합하면 된다!
+	//ex) 백슬래시(\) 출력하려면 \\
+	//ex) 큰따옴표(") 출력하려면 앞에 \
+	
+	std::cout << "|\\_/|" << std::endl;
+	std::cout << "|q p|   /}" << std::endl;
+	std::cout << "( 0 )\"\"\"\\" << std::endl;
+	std::cout << "|\"^\"`    |" << std::endl;
+	std::cout << "||_/=\\\\__|" << std::endl;
+
+	return 0;
+}
