3c64ee16cfb1221c2dbd492f239f8d411691e2b6
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-01 14:20:18 +0900

    Refactor min/max calculation logic
    Refactor variable names and improve code readability.

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -5,29 +5,19 @@ using namespace std;
 int main() {
 
 	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(0);
 
-	int n;
-	int num;
+	int n, input;
+	int min = 1000000, max=-1000000; //최솟값, 최댓값 범위
 	cin >> n;
 
-	int minvalue = 1000000;
-	int maxvalue = -1000000;
-
 	for (int i = 0; i < n; i++) {
-		cin >> num;
+		cin >> input;
 
-		if (num > maxvalue) {
-			maxvalue = num;
-		}
-		if (num < minvalue) {
-			minvalue = num;
-		}
+		if (input < min) min = input; //min보다 작은 수 -> 대입하여 최솟값을 구함
+		if (input > max) max = input; //max보다 큰 수 -> 대입하여 최댓값을 구함
 	}
-
-	cout << minvalue << " " << maxvalue;
+	cout << min << " " << max;
 
 	return 0;
-
-
 }
