c089da9c998ee04713bdb2c3a5e404b5094d4f6d
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-01 18:29:30 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	int num, count=10;
+	int array[10] = {0, };
+
+	for (int i = 0; i < 10; i++) {
+		cin >> num;
+		array[i] = num % 42;
+
+		for (int j = 0; j < i; j++) {
+			if (array[i] == array[j] ) {
+				count -= 1;
+				break;
+			}
+		}
+	}
+
+	cout << count;
+
+	return 0;
+}
