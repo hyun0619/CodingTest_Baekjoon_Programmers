f0de40bbc85be1c6bc37909a105b5d8df0ad8395
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-02 19:43:43 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -7,23 +7,21 @@ int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(0);
 
-	int n;
-	cin >> n;
+	int m;
+	cin >> m;
 
-	double score[1000] = {0};
-	double max = 0, total = 0;
+	double score[1000] = { 0 };
+	double max=0, average=0;
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < m; i++) {
 		cin >> score[i];
-		
-		if (score[i] > max)	max = score[i];
+
+		if (score[i] > max) max = score[i];
 	}
 
-	for (int i = 0; i < n; i++) {
-		total += score[i] / max * 100;
+	for (int i = 0; i < m; i++) {
+		average += score[i] / max * 100;
 	}
 
-	cout << total / n;
-	return 0;
-
+	cout << average/m;
 }
