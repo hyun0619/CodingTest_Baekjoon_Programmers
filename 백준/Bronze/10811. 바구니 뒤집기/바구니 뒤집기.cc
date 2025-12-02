1786b723f017da07acd95b1b37b68c8822ac8543
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-02 19:36:46 +0900

    Refactor code with comments for better understanding
    Added comments for clarity and maintained code structure.

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -21,9 +21,10 @@ int main() {
 		int begin, end;
 		cin >> begin >> end;
 
-		begin--, end--;
-
-		for (int i = 0; i <= (end - begin) / 2; i++) {
+		begin--, end--; //배열 인덱스가 0부터 시작, 계산 용이하기 위해 1 빼주기
+		
+		//if (begin != end) 하지 않아도 괜찮음!
+		for (int i = 0; i <= (end - begin) / 2; i++) { //양끝에서 한쌍씩 교체하기 때문에 나누기 2
 			swap(basket[begin + i], basket[end - i]);
 		}
 	}
