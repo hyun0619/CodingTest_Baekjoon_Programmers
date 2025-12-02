64607ec8329579404b30cce3e6e98fc6105e9735
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-02 19:34:17 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -7,32 +7,29 @@ int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(0);
 
-	int basketamount, changenum;
-	cin >> basketamount >> changenum;
+	int n, m;
+	cin >> n >> m;
 
 	int basket[101] = { 0 };
 
-	//바구니 번호 지정
-	for (int i = 0; i < basketamount; i++) {
+	for (int i = 0; i < n; i++) {
 		basket[i] = i + 1;
 	}
 
-	while (changenum--) { //횟수가 0(false)가 될 때까지
+	while (m--) {
+
 		int begin, end;
 		cin >> begin >> end;
-		
-		//배열은 [0]부터 시작하기 때문에 계산을 위해 1을 빼주는 작업
-		begin--;
-		end--;
 
-		//begin과 end가 같다면 변화x, 달라야 역순 작업 진행 가능
-		if (begin != end) {
-			for (int i = 0; i <= (end - begin) / 2; i++) { //양끝 한쌍씩 바꾸니까 나누기 2
-				swap(basket[begin + i], basket[end - i]); // 각 끝에서부터 한쌍씩 자리 바꾸기
-			}
+		begin--, end--;
+
+		for (int i = 0; i <= (end - begin) / 2; i++) {
+			swap(basket[begin + i], basket[end - i]);
 		}
 	}
 
-	for (int i = 0; i < basketamount; i++) cout << basket[i] << " ";
+	for (int i = 0; i < n; i++) cout << basket[i] << " ";
+
 	return 0;
+
 }
