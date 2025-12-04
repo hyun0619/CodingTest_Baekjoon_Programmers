6e28f93f1e346be3fc3f4c48b4ecf857149fc0ed
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-04 10:09:54 +0900

    Include string header and modify output format
    Added #include <string> and adjusted output formatting.

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -10,9 +11,9 @@ int main() {
 
 	for (char i = 'a'; i <= 'z'; i++) { //a~z까지 char로 불러올 수 있음
 		
-		//find_입력한 문자열이 최초로 등장하는 위치의 인덱스를 반환
+		//string 맴버함수 find_입력한 문자열이 최초로 등장하는 위치의 인덱스를 반환
 		//없는 문자열은 string::npos가 반환, 자료형이 unsigned라 int로 캐스팅
-		cout << (int)s.find(i) << endl;
+		cout << (int)s.find(i) << " ";
 	}
 
 	return 0;
