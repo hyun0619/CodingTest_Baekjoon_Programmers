3ef24d21158744bd2c662b24996277b99465ac69
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-01-14 09:44:31 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -7,14 +7,13 @@ using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
-    for (int i=0; i<commands.size();i++) {
+    for (int i=0;i<commands.size();i++) {
         vector<int> tmp;
-        for (int j=commands[i][0]-1; j<=commands[i][1]-1; j++) {
-            tmp.push_back(array[j]);
+        for (int j=commands[i][0]; j<=commands[i][1]; j++) {
+            tmp.push_back(array[j-1]); //[i][0]번~[i][1]번까지 넣기
+            sort(tmp.begin(), tmp.end()); //정렬
         }
-        sort(tmp.begin(), tmp.end());
-        answer.push_back(tmp[commands[i][2]-1]);
+        answer.push_back(tmp[commands[i][2]-1]); //[i][2]번째 수 구하기
     }
-    
     return answer;
 }
