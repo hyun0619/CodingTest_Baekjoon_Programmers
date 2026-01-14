4cecd65b91b91c20faac3bdd46f40f8763e0ec96
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-01-14 10:04:30 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 vector<string> solution(vector<string> strings, int n) {
     
-    sort(strings.begin(), strings.end(), [n](string a, string b) {
+    //정렬 기준 부분에서 람다 함수 사용
+    //복사 비용 때문에 const 참조자로 받는 게 실무에선 유리
+    sort(strings.begin(), strings.end(), [n](const string& a, const string& b) {
+        //n번째 문자 같은 경우 - 전체 문자열 기준_오름차순 정리
         if (a[n] == b[n]) return a<b;
-        return a[n]<b[n];
+        //n번째 문자 다른 경우 - n번째 문자 기준_오름차순 정리
+        return a[n] < b[n];
     });
     
     return strings;
