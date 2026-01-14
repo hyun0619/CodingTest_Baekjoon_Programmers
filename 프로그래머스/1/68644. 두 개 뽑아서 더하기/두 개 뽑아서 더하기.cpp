f2e0511bae897d40c84ef22e15c3f15e179777f1
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-01-14 09:21:42 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.20 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -7,13 +7,18 @@ using namespace std;
 vector<int> solution(vector<int> numbers) {
     vector<int> answer;
     
-    for (int i=0; i< numbers.size(); i++) {
-        for (int j=i+1; j<numbers.size();j++) {
-            answer.push_back(numbers[i] + numbers[j]);
-            }
+    //더할 수 있는 모든 경우의 수 구하기
+    for (int i=0;i<numbers.size(); i++) {
+        for (int j=i+1;j<numbers.size(); j++) {
+            answer.push_back(numbers[i]+numbers[j]);
         }
+    }
+    //unique 함수 사용을 위해 정렬
     sort(answer.begin(), answer.end());
+    //unique 함수_중복 숫자 제거
+    //erase 함수_빈 자리에 생긴 쓰레기 값 제거 
     answer.erase(unique(answer.begin(), answer.end()), answer.end());
+    
     
     return answer;
 }
