93e07811eb520a75d2c8a747f5ade6d92ca97899
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-01-21 09:35:05 +0900

    [level 1] Title: 모의고사, Time: 0.09 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,18 +1,15 @@
-#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
-    vector<int> answer;
-    vector<int> allcase(3, 0);
-    vector<int> tmp;
+    vector<int> answer, allcase(3, 0);
     vector<int> case1 = {1, 2, 3, 4, 5};
     vector<int> case2 = { 2, 1, 2, 3, 2, 4, 2, 5};
     vector<int> case3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
-    int num = 0;
     
+    //정답 개수 찾기
     for (int i=0; i<answers.size(); i++) {
         if (case1[i%case1.size()] == answers[i]) {
             allcase[0] += 1;
@@ -25,6 +22,8 @@ vector<int> solution(vector<int> answers) {
         }
     }
     
+    //범위 내에서 가장 큰 값을 가리키는 반복자 반환
+    //*을 붙여서 값을 가져옴
     int max_num = *max_element(allcase.begin(), allcase.end());
     
     for (int j=0; j<allcase.size(); j++) {
@@ -32,7 +31,6 @@ vector<int> solution(vector<int> answers) {
             answer.push_back(j+1);
         }
     }
-    
 
     return answer;
 }
