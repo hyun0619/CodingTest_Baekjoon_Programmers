974187c9d79a01e62c9d179473ea2476c4452fca
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-01-27 09:42:44 +0900

    [level 1] Title: 옹알이 (2), Time: 0.11 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -7,14 +7,19 @@ int solution(vector<string> babbling) {
     int answer = 0;
     
     for (int i=0; i<babbling.size(); i++) {
-        bool check=false;
+        //기본을 true, 발음 불가능한 부분이 나오면 false로 바꿀 '실패 플래그'
+        bool check=true;
+        //바로 직전에 발음한 단어 기억 변수 == 연속 발음 방지
+        //현재 체크 중인 글자
         string first = "", fb = "";
         
+        //단어의 글자 하나하나 체크
         for (int j=0; j<babbling[i].size(); j++) {
+            //현재 검사할 글자를 문자열로 변환해 저장
             fb = babbling[i][j];
             if (babbling[i].substr(j, 3) == "aya" && first != fb) {
                 j+=2;
-                first = fb;
+                first = fb; //방금 발음했음을 기록
             }
             else if (babbling[i].substr(j, 2) == "ye" && first != fb) {
                 j+=1;
@@ -28,10 +33,11 @@ int solution(vector<string> babbling) {
                 j+=1;
                 first = fb;
             }
-            else check = true;
+            //4개 경우 해당X면 발음 불가
+            else check = false; //실패라는 의미!
         }
-        
-        if (!check) answer++;
+        //단어 끝까지 검사했는데 실패한 적 없으면 성공
+        if (check) answer++;
     }
     
     return answer;
