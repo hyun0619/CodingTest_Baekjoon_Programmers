14ba2700ddab0679df3f9e28c4c172853618c065
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-16 09:48:50 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+    vector<int> answer = {0, 0};
+    
+    for (int i=0; i<array.size(); i++) {
+        if (answer[0] < array[i]) {
+            answer[0] = array[i];
+            answer[1] = i;
+        }
+    }
+    
+    return answer;
+}
