a7d45bc0e656aa918afce834add2906ad361b65c
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-18 09:32:08 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> left, answer;
+    
+    for(int i = 0; i < progresses.size(); ++i) {
+        int more = (100 - progresses[i]) / speeds[i];
+        int rest = (100 - progresses[i]) % speeds[i];
+        if(rest > 0) more++;
+        left.push_back(more);
+    }
+    int max_num = left[0], count = 1;
+    for(int i = 1; i < left.size(); ++i) {
+        if(max_num >= left[i]) count++;
+        else {
+            answer.push_back(count);
+            max_num = left[i];
+            count = 1;
+        }
+    }
+    answer.push_back(count);
+    return answer;
+}
