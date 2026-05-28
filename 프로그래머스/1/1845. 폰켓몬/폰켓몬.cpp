91c6a17f8623877c5b8b6d043e463e4dcb3e5159
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-05-28 09:24:04 +0900

    [level 1] Title: 폰켓몬, Time: 0.44 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -5,15 +5,18 @@ using namespace std;
 
 int solution(vector<int> nums)
 {
+    // 전체값/2
     int max = nums.size()/2;
-    
+    // 중복 없이 담아둘 곳
     unordered_set<int> types;
     
+    // 중복 제거
     for (int i : nums) {
         types.insert(i);
     }
     
     int count = types.size();
     
+    // 둘 중 더 작은 값이 답
     return min(max, count);
 }
